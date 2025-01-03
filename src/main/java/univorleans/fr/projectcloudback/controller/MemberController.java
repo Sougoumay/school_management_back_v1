b538ce44844599e3787854f07e47adcaa8a9a9c2
Sougoumay <elidjaihamid]gmail.com>
2025-01-03 21:22:47 +0100

    la modif de l'acr

--- src/main/java/univorleans/fr/projectcloudback/controller/MemberController.java
+++ src/main/java/univorleans/fr/projectcloudback/controller/MemberController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import univorleans.fr.projectcloudback.model.Member;
 
